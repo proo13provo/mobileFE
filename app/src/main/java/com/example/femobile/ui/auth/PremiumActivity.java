9d6eae1f4350e510d0a0f416a8215f053f8ddd36
Viet <kiritoak01@gmail.com>
2025-06-05 16:45:52 +0700

    thêm lớp java premium

---
+++ app/src/main/java/com/example/femobile/ui/auth/PremiumActivity.java
@@ -0,0 +1,16 @@
+package com.example.femobile.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.femobile.R;
+
+public class PremiumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_premium);
+    }
+} 
